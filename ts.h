346a210d1c104b616a7ef906e5aa40631d0d0fbf
Rayane Rafik Mt <rayanerafikmt@MacBook-Pro-de-Rayane.local>
2024-12-12 18:19:04 +0100

    modif lexical ts

--- ts.h
+++ ts.h
@@ -2,88 +2,212 @@
 #include <stdlib.h>
 #include <string.h>
 #include "syntax.tab.h"
-// extern YYSTYPE yylval;
-// extern int nb_ligne;
-// extern int col;
-typedef struct SymbT {
-    char* nom;
-    char* type;
-    struct SymbT* suivant;
-} SymbT;
-typedef struct Symbole {
-    char* texte;
-    struct Symbole* suivant;
-} Symbole;
+#define max 20
 
-typedef struct ListeBinaire {
-    int num;
-    Symbole* liste;
-    struct ListeBinaire* suivant;
-} ListeBinaire;
+typedef struct liste
+{
+    char entite[max];
+    struct liste *suivant;
+} liste;
 
-SymbT* listeIdentifiants = NULL;
-SymbT* listeCst = NULL;
-Symbole* listeMotsCles = NULL;
-Symbole* listeOperateurs = NULL;
-Symbole* listeLogiques = NULL;
-Symbole* listeTypes = NULL;
-ListeBinaire* listeBinaire = NULL;
-void insererIdentifiant(char* nom, char* type) {
-    SymbT* nouvelIdentifiant = (SymbT*)malloc(sizeof(SymbT));
-    if (nouvelIdentifiant) {
-        nouvelIdentifiant->nom = strdup(nom);
-        nouvelIdentifiant->type = strdup(type);
-        nouvelIdentifiant->suivant = listeIdentifiants;
-        listeIdentifiants = nouvelIdentifiant;
+typedef struct constant{
+    char type[max];
+    union {
+        int i;             
+        float f;           
+        char *s;           
+    } valeur;  
+}constant;
+typedef struct listeD
+{
+    char entite[max];
+    char type[max];
+    int is_const;
+    union {
+        int i;          
+        float f;           
+        char *s;           
+    } valeur;  
+    struct listeD *suivant;
+} listeD;
+
+typedef struct listeT
+{
+    char entite[max];
+    char type[max];
+    int taille;
+    union {
+        int i;          
+        float f;           
+        char *s;           
+    } valeur;  
+    struct listeT *suivant;
+} listeT;
+liste *ListeMotCle = NULL;
+liste *ListeOperateurLogique = NULL;
+liste *ListeOperateurArithmetique = NULL;
+liste *ListeOperateurNoAsso = NULL;
+listeD *TS = NULL;
+listeT *TStab = NULL;
+
+void insererMotCle(char tete[])
+{
+    liste *nouveau = (liste *)malloc(sizeof(liste));
+    if (nouveau)
+    {
+        strcpy(nouveau->entite, tete);
+        nouveau->suivant = ListeMotCle;
+        ListeMotCle = nouveau;
+    } 
+    else {
+        printf("erreur");
     }
 }
 
-void insererCst(char* nom, char* type) {
-    SymbT* nouvelIdentifiant = (SymbT*)malloc(sizeof(SymbT));
-    if (nouvelIdentifiant) {
-        nouvelIdentifiant->nom = strdup(nom);
-        nouvelIdentifiant->type = strdup(type);
-        nouvelIdentifiant->suivant = listeCst;
-        listeCst = nouvelIdentifiant;
+void insererOperateurLogique(char tete[])
+{
+    liste *nouveau = (liste *)malloc(sizeof(liste));
+    if (nouveau)
+    {
+        strcpy(nouveau->entite, tete);
+        nouveau->suivant = ListeOperateurLogique;
+        ListeOperateurLogique = nouveau;
+    }else {
+        printf("erreur");
     }
 }
 
-void insererDansListeBinaire(int code, const char* texte) {
-    Symbole* nouveauSymbole = (Symbole*)malloc(sizeof(Symbole));
-    if (nouveauSymbole) {
-        nouveauSymbole->texte = strdup(texte);
-        nouveauSymbole->suivant = NULL;
+void insererOperateurArithmetique(char tete[])
+{
+    liste *nouveau = (liste *)malloc(sizeof(liste));
+    if (nouveau)
+    {
+        strcpy(nouveau->entite, tete);
+        nouveau->suivant = ListeOperateurArithmetique;
+        ListeOperateurArithmetique = nouveau;
+    }else {
+        printf("erreur");
+    }
+}
 
-        ListeBinaire* current = listeBinaire;
-        while (current) {
-            if (current->num == code) {
-                nouveauSymbole->suivant = current->liste;
-                current->liste = nouveauSymbole;
-                return;
-            }
-            current = current->suivant;
+void insererOperateurNoAsso(char tete[])
+{
+    liste *nouveau = (liste *)malloc(sizeof(liste));
+    if (nouveau)
+    {
+        strcpy(nouveau->entite, tete);
+        nouveau->suivant = ListeOperateurNoAsso;
+        ListeOperateurNoAsso = nouveau;
+    }else {
+        printf("erreur");
+    }
+}
+int rechercherIdf(char entite[]) {
+    listeD *current = TS; 
+    while (current != NULL) {
+        if (strcmp(current->entite, entite) == 0) {
+            return 1; 
         }
-
-        ListeBinaire* nouvelleListeBinaire = (ListeBinaire*)malloc(sizeof(ListeBinaire));
-        nouvelleListeBinaire->num = code;
-        nouvelleListeBinaire->liste = nouveauSymbole;
-        nouvelleListeBinaire->suivant = listeBinaire;
-        listeBinaire = nouvelleListeBinaire;
+        current = current->suivant; 
     }
+    listeT *currentTab = TStab;
+    while (currentTab != NULL)
+    {
+        if((strcmp(currentTab->entite, entite) == 0)){
+            return 1;
+        }
+        currentTab = currentTab->suivant;
+    }
+    return 0;
+}
+int insererTableau(char entite[], char type[], int taille, void *valeur) {
+    listeT *current = TStab;
+    if (rechercherIdf(entite)==1)
+    {
+        return -1;
+    }
+    // while (current != NULL) {
+    //     if (strcmp(current->entite, entite) == 0) {
+    //         fprintf(stderr, "Erreur: Le symbole '%s' est déjà déclaré.\n", entite);
+    //         exit(1);
+    //     }
+    //     current = current->suivant;
+    // }
+
+    listeT *nouveau = (listeT *)malloc(sizeof(listeT));
+    if (nouveau) {
+        strcpy(nouveau->entite, entite);
+        strcpy(nouveau->type, type);
+        nouveau->taille = taille;
+        if (strcmp(type, "NUM") == 0) {
+            nouveau->valeur.i = *(int *)valeur; //*(int *) = void 
+        } 
+        else if (strcmp(type, "REAL") == 0) {
+            nouveau->valeur.f = *(float *)valeur; 
+        }
+        else if (strcmp(type, "TEXT") == 0) {
+            nouveau->valeur.s = (char *)malloc(strlen((char *)valeur) +1);
+            if (nouveau->valeur.s) {
+                strcpy(nouveau->valeur.s, (char *)valeur); 
+            }
+        } 
+        nouveau->suivant = TStab;
+        TStab = nouveau;
+    } else {
+        fprintf(stderr, "Erreur d'allocation mémoire pour le symbole '%s'\n", entite);
+        exit(1);
+    }
+    return 1;
+}
+int insererTS(char entite[], char type[], int is_const, void *valeur) {
+    listeD *current = TS;
+    if(rechercherIdf(entite)==1)
+    {
+        return -1;
+    }
+    while (current != NULL) {
+        if (strcmp(current->entite, entite) == 0) {
+            fprintf(stderr, "Erreur: Le symbole '%s' est déjà déclaré.\n", entite);
+            exit(1);
+        }
+        current = current->suivant;
+    }
+
+    listeD *nouveau = (listeD *)malloc(sizeof(listeD));
+    if (nouveau) {
+        strcpy(nouveau->entite, entite);
+        strcpy(nouveau->type, type);
+        nouveau->is_const = is_const;
+        if (strcmp(type, "NUM") == 0) {
+            nouveau->valeur.i = *(int *)valeur; 
+        } 
+        else if (strcmp(type, "REAL") == 0) {
+            nouveau->valeur.f = *(float *)valeur; 
+        }
+        else if (strcmp(type, "TEXT") == 0) {
+            char **str_ptr = (char**)valeur; 
+            if (str_ptr && *str_ptr) {  
+                size_t len = strlen(*str_ptr);
+                nouveau->valeur.s = (char *)malloc(len + 1);
+                if (nouveau->valeur.s) 
+                {
+                    strcpy(nouveau->valeur.s, *str_ptr);
+                    printf("Chaîne copiée : %s\n", nouveau->valeur.s);
+                }
+            }   else {
+                    fprintf(stderr, "Erreur : Pointeur de chaîne invalide pour '%s'\n", entite);
+                    free(nouveau);
+                    return -1;
+            }
+}
+        nouveau->suivant = TS;
+        TS = nouveau;
+    } 
+    else {
+        fprintf(stderr, "Erreur d'allocation mémoire pour le symbole '%s'\n", entite);
+        exit(1);
+    }
+    return 1;
 }
 
-void insererMotCle(const char* motCle) {
-    insererDansListeBinaire(1, motCle);
-}
 
-void insererOperateur(const char* operateur) {
-    insererDansListeBinaire(2, operateur);
-}
-
-void insererOperateurLogique(const char* operateurLogique) {
-    insererDansListeBinaire(3, operateurLogique);
-}
-
-void insererType(const char* type) {
-    insererDansListeBinaire(4, type);
-}
